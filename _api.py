69afe580670e47e49d39ed28dff04441e815c0fd
Kévin Naudin <naudin.kevin@outlook.fr>
2019-04-15 16:22:32 +0200

    [Weather] Added a manual script to retrieve weather information based on a given address.

---
+++ _api.py
@@ -0,0 +1,1 @@
+API_KEY = "you're a big dumdum if you thought I'd left it here'
---
+++ get_weather.py
@@ -0,0 +1,39 @@
+import hjson
+import requests
+import os
+
+from time import time
+from _api import API_KEY
+
+BUILD_ADDRESS = True
+config = {}
+
+if os.path.exists('config.info'):
+    BUILD_ADDRESS = False
+    with open('config.info', 'r') as info_file:
+        config = hjson.load(info_file)
+    if not config['address']:
+        BUILD_ADDRESS = True
+
+if BUILD_ADDRESS:
+    data = []
+    while not data:
+        print('Enter your address with no commas or abreviations :')
+        address = input().replace(' ', '+')
+        r = requests.get('https://nominatim.openstreetmap.org/search?q={}&format=json'.format(address))
+        data = hjson.loads(r.text)
+        if not data:
+            print('Invalid address. Try again.')
+    with open('config.info', 'w') as info_file:
+        print('Address confirmed. Configuring new address.')
+        config['address'] = address
+        config['lat'] = data[0]['lat']
+        config['lon'] = data[0]['lon']
+        info_file.write('{\n    address : "'+address+'",\n    lat : '+config['lat']+'\n    lon : '+config['lon']+'\n}')
+
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units=metric&appid={}'.format(config['lat'], config['lon'], API_KEY))
+if r.status_code is 200:
+    weather = json.loads(r.text)
+    weather['timestamp'] = time()
+    with open('weather.json', 'w') as f:
+        hjson.dump(weather, f)
